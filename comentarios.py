fe9f5afb68f2b6b1a00039f7b29f54366d7ab25e
LvegR11 <luis.vega970386@gmail.com>
2024-09-30 23:02:58 -0500

    Comentarios

---
+++ comentarios.py
@@ -0,0 +1,25 @@
+#punto 3 de la actividad 1
+
+#Declaro la variable que contiene el nombre de mi comida favorita
+comida_favorita = "espaguetis" 
+
+#Imprimo la variable con el valor asignado en consola
+print("Mi comida favorita es el", comida_favorita) 
+
+
+#punto 4 de la actividad 1
+
+#Declaro la variable que contiene el dinero disponible en mi cartera
+dinero_disponible = 20000
+
+#Imprimo la variable con el valor asignado en consola
+print("Actualmete tengo", dinero_disponible,"pesos en mi cartera")
+
+
+#punto 5 de la actividad 1
+
+#Declaro la variable que indica si me gusta el clima frio
+Clima_frio = True
+
+#Imprimo la variable con el valor asignado en consola
+print("Me gusta el clima frio: ", Clima_frio)
---
+++ punto3-4-5.py
@@ -0,0 +1,13 @@
+#punto 3 de la actividad 1
+comida_favorita = "espaguetis"
+print("Mi comida favorita es el", comida_favorita)
+
+
+#punto 4 de la actividad 1
+dinero_disponible = 20000
+print("Actualmete tengo", dinero_disponible,"pesos en mi cartera")
+
+
+#punto 5 de la actividad 1
+Clima_frio = True
+print("Me gusta el clima frio: ", Clima_frio)
